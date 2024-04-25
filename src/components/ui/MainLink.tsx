aa522f9177d8e85002d7a0514bc74cce433373c6
didoslavov <didkoslawow@gmail.com>
2024-04-26 01:00:45 +0300

    add active state to main link and fix styles

--- src/components/ui/MainLink.tsx
+++ src/components/ui/MainLink.tsx
@@ -5,17 +5,22 @@ import { BiChevronRight } from "react-icons/bi";
 function MainLink({
   children,
   className,
+  active,
 }: {
   children: ReactNode;
   className?: string;
+  active?: boolean;
 }) {
   return (
     <Link
       href="#"
-      className="relative transition-all duration-300 before:absolute before:bottom-0 before:left-0 before:h-0 before:w-full before:rounded-[4px] before:bg-main-blue-900 before:opacity-20 before:transition-all before:duration-500 hover:before:h-full"
+      className={`relative font-medium transition-all duration-300 before:absolute before:bottom-0 before:left-0 before:h-0 before:w-full before:rounded-[4px] ${active ? "before:bg-main-green" : "before:bg-main-blue-900"} before:opacity-20 before:transition-all before:duration-500 hover:before:h-full`}
     >
       {children}
-      <BiChevronRight className={`inline-block ${className}`} size={26} />
+      <BiChevronRight
+        className={`inline-block ${active && "text-main-green"} ${className}`}
+        size={26}
+      />
     </Link>
   );
 }
