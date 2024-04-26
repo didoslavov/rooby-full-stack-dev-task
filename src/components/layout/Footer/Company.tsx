2bb18ed2950d0a699dfcca236a473016d169a10d
didoslavov <didkoslawow@gmail.com>
2024-04-26 21:22:16 +0300

    add company navigation

--- src/components/layout/Footer/Company.tsx
+++ src/components/layout/Footer/Company.tsx
@@ -5,7 +5,7 @@ function Company() {
   const data = getFooterLinks("company");
 
   return (
-    <article>
+    <article className="col-start-4 row-start-2 justify-self-end sm:col-start-6 lg:row-start-1 xl:col-start-5">
       <NavContainer data={data} />
     </article>
   );
