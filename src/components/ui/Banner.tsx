fca55825ed9d47847f0ef8f04e84006f6ef3abb5
didoslavov <didkoslawow@gmail.com>
2024-04-26 14:53:47 +0300

    fix responsive styles

--- src/components/ui/Banner.tsx
+++ src/components/ui/Banner.tsx
@@ -19,8 +19,8 @@ function Banner() {
         </p>
       </div>
       <div className="pointer-events-none z-30 col-start-8 col-end-13 row-start-1 bg-main-yellow pr-[5%]"></div>
-      <div className="pointer-events-none absolute -bottom-[30%] -right-[10%] z-50 flex h-72 w-72 flex-col items-center rounded-[50%] bg-main-green transition-all duration-500 hover:bg-main-green-100 md:-bottom-[60%] md:h-96 md:w-96 xl:-bottom-[70%] xl:-right-[10%] xl:h-[800px] xl:w-[800px] xl:translate-y-20">
-        <div className="peer pointer-events-auto mb-6 mt-10 flex w-fit rounded-lg bg-white px-4 py-2 text-sm font-bold text-black drop-shadow-2xl transition-all duration-500 hover:cursor-pointer hover:bg-main-green xl:mr-20 xl:mt-32 xl:items-center xl:gap-4 xl:px-8 xl:py-5 xl:text-lg xl:-tracking-[0.25px]">
+      <div className="pointer-events-none absolute -bottom-[30%] -right-[5%] z-50 flex h-72 w-80 flex-col items-center rounded-[50%] bg-main-green transition-all duration-500 hover:bg-main-green-100 sm:-bottom-[40%] md:-bottom-[60%] md:-right-[10%] md:h-96 md:w-96 xl:-bottom-[70%] xl:-right-[10%] xl:h-[800px] xl:w-[800px] xl:translate-y-20">
+        <div className="peer pointer-events-auto mb-6 mt-10 flex w-fit items-center rounded-lg bg-white px-4 py-2 text-sm font-bold text-black drop-shadow-2xl transition-all duration-500 hover:cursor-pointer hover:bg-main-green xl:mr-20 xl:mt-32 xl:gap-4 xl:px-8 xl:py-5 xl:text-lg xl:-tracking-[0.25px]">
           <Link href="#">Try For Free</Link>
           <BiChevronRight size={28} />
         </div>
