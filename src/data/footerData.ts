df3a06abe578450442730132574f6b57fec92af2
didoslavov <didkoslawow@gmail.com>
2024-04-26 16:42:38 +0300

    add footer links data

---
+++ src/data/footerData.ts
@@ -0,0 +1,36 @@
+export default function getFooterLinks(title: string) {
+  const links = [
+    {
+      title: "Company",
+      links: ["About", "Pricing", "Jobs", "Blog", "Careers"],
+    },
+    {
+      title: "Product",
+      links: [
+        "Sales software",
+        "Features",
+        "Privacy",
+        "Marketplace",
+        "Status",
+        "API",
+      ],
+    },
+    {
+      title: "Discover",
+      links: [
+        "Partner Program",
+        "Get our newsletter",
+        "Sales Pipeline ",
+        "What is CRM?",
+        "CRM Comparison",
+        "CarResourceseers",
+      ],
+    },
+    {
+      title: "Help Center",
+      links: ["Community", "Knowledge Base", "Academy", "Support"],
+    },
+  ];
+
+  return links.filter((l) => l.title.toLowerCase() === title.toLowerCase())[0];
+}
