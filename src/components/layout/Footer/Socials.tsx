d9c0176e543cb4660a3997d534b1deb76eef4e42
didoslavov <didkoslawow@gmail.com>
2024-04-26 16:43:34 +0300

    add socials component

---
+++ src/components/layout/Footer/Socials.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import logo from "/public/logo.svg";
+import twitter from "/public/footer/twitter.svg";
+import facebook from "/public/footer/facebook.svg";
+import linkedin from "/public/footer/linkedin.svg";
+
+function Socials() {
+  return (
+    <article className="xl:col-span-3 xl:pb-[100px] xl:pr-20">
+      <Image src={logo} alt="Company Logo" className="mb-[26px]" />
+      <div className="mb-6 text-sm text-main-grey">
+        <p className="leading-6">We built an elegant solution. </p>
+        <p className="text-wrap leading-6">
+          Our team created a fully integrated sales and marketing solution for
+          SMBs
+        </p>
+      </div>
+      <div className="flex gap-4">
+        <Image src={twitter} alt="Twitter Logo" />
+        <Image src={facebook} alt="Facebook Logo" />
+        <Image src={linkedin} alt="Linked In Logo" />
+      </div>
+    </article>
+  );
+}
+
+export default Socials;
