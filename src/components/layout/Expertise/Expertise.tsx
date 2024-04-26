2400d847bc051e9023b47693b6b1a418164a55e4
didoslavov <didkoslawow@gmail.com>
2024-04-27 01:06:18 +0300

    add cta link to marketing card

--- src/components/layout/Expertise/Expertise.tsx
+++ src/components/layout/Expertise/Expertise.tsx
@@ -1,8 +1,8 @@
-import React, { Fragment } from "react";
 import increase from "/public/expertise/increase.svg";
 import marketing from "/public/expertise/marketing.svg";
 import help from "/public/expertise/help.svg";
 import Card from "./Card";
+import Link from "next/link";
 
 const cards = [
   {
@@ -46,14 +46,29 @@ const cards = [
 function Expertise() {
   return (
     <section className="col-start-1 col-end-13 grid grid-cols-subgrid bg-white">
-      {cards.map((c, i) => (
-        <article
-          key={i}
-          className={`flex flex-col items-center gap-10 sm:text-center md:flex-row md:text-start lg:gap-0 ${i === cards.length - 1 && "mb-16 lg:mb-[120px]"} ${i % 2 === 1 ? "xl:flex-row-reverse xl:gap-24" : "xl:flex-row"} col-start-2 col-end-12 mt-16 border-t border-main-grey-700 xl:mt-[120px] xl:pt-12`}
-        >
-          <Card expertise={c} />
-        </article>
-      ))}
+      {cards.map((c, i) => {
+        return (
+          <article
+            key={i}
+            className={`relative flex flex-col items-center gap-10 sm:text-center md:flex-row md:text-start lg:gap-0 ${i === cards.length - 1 && "mb-16 lg:mb-[120px]"} ${i % 2 === 1 ? "xl:flex-row-reverse xl:gap-24" : "xl:flex-row"} col-start-2 col-end-12 mt-16 border-t border-main-grey-700 xl:mt-[120px] xl:pt-12`}
+          >
+            <Card expertise={c} />
+            {i === 1 && (
+              <div className="pointer-events-none absolute -bottom-10 flex flex-col items-center gap-3 rounded-xl bg-main-blue py-3 pl-6 pr-3 text-base text-white hover:bg-main-yellow hover:text-black sm:bottom-8 sm:left-10 sm:flex-row md:-bottom-10 md:left-40 lg:-bottom-14 lg:left-56 lg:text-lg xl:bottom-10 xl:left-14">
+                <p className=" font-bold -tracking-[0.25px]">
+                  Automate your sales & marketing
+                </p>
+                <Link
+                  href="#"
+                  className="pointer-events-auto rounded-lg bg-main-green p-2 font-bold -tracking-[0.25px] hover:bg-main-green-100 lg:pb-4 lg:pl-5 lg:pr-[18px] lg:pt-4"
+                >
+                  Start now
+                </Link>
+              </div>
+            )}
+          </article>
+        );
+      })}
     </section>
   );
 }
