64740a08a5f27acb3d07b7e7cb9013bbb40d479d
didoslavov <didkoslawow@gmail.com>
2024-04-26 21:22:42 +0300

    add produc navigation

--- src/components/layout/Footer/Product.tsx
+++ src/components/layout/Footer/Product.tsx
@@ -1,12 +1,11 @@
 import NavContainer from "@/components/ui/NavContainer";
 import getFooterLinks from "@/data/footerData";
-import React from "react";
 
 function Product() {
   const data = getFooterLinks("product");
 
   return (
-    <article className="col-span-2 col-start-5 justify-self-end">
+    <article className="col-span-2 col-start-6 row-start-2 justify-self-end sm:col-start-7 lg:row-start-1 xl:col-start-6">
       <NavContainer data={data} />
     </article>
   );
