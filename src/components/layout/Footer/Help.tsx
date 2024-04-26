6a783d457f573e9473950839cbed8eef26c0885d
didoslavov <didkoslawow@gmail.com>
2024-04-26 21:22:34 +0300

    add help navigation

--- src/components/layout/Footer/Help.tsx
+++ src/components/layout/Footer/Help.tsx
@@ -6,7 +6,7 @@ function Help() {
   const data = getFooterLinks("help center");
 
   return (
-    <article className="col-start-9 col-end-11 justify-self-center">
+    <article className="col-start-8 row-start-2 text-nowrap sm:col-start-9 md:col-start-10 lg:col-start-11 lg:row-start-1 xl:col-start-10 xl:ml-10">
       <NavContainer data={data} />
     </article>
   );
