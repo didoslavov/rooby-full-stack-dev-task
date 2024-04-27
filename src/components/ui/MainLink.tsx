4faf673eaa189d9d0c173f3848ab877934c9be1a
didoslavov <didkoslawow@gmail.com>
2024-04-28 00:44:40 +0300

    add params to main link component

--- src/components/ui/MainLink.tsx
+++ src/components/ui/MainLink.tsx
@@ -2,25 +2,35 @@ import Link from "next/link";
 import React, { ReactNode } from "react";
 import { BiChevronRight } from "react-icons/bi";
 
+/**
+ * @param { boolean} plane Renders the link without arrow icon.
+ */
+
 function MainLink({
   children,
   className,
   active,
+  plane,
+  link,
 }: {
   children: ReactNode;
   className?: string;
   active?: boolean;
+  plane?: boolean;
+  link: string;
 }) {
   return (
     <Link
-      href="#"
-      className={`relative font-medium transition-all duration-300 before:absolute before:bottom-0 before:left-0 before:h-0 before:w-full before:rounded-[4px] ${active ? "before:bg-main-green" : "before:bg-main-blue-900"} before:opacity-20 before:transition-all before:duration-500 hover:before:h-full`}
+      href={link}
+      className={`${className} relative font-medium transition-all duration-300 before:absolute before:bottom-0 before:left-0 before:h-0 before:w-full before:rounded-[4px] ${active ? "before:bg-main-green" : "before:bg-main-blue-900"} before:opacity-20 before:transition-all before:duration-500 hover:before:h-full`}
     >
       {children}
-      <BiChevronRight
-        className={`inline-block ${active && "text-main-green"} ${className}`}
-        size={26}
-      />
+      {!plane && (
+        <BiChevronRight
+          className={`inline-block ${active && "text-main-green"}`}
+          size={26}
+        />
+      )}
     </Link>
   );
 }
