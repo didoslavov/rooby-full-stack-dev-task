7c3ab71d3d15aaaeb68537eab6986f034c9aa3b6
didoslavov <didkoslawow@gmail.com>
2024-04-27 15:36:30 +0300

    fix styles

--- src/components/ui/Chip.tsx
+++ src/components/ui/Chip.tsx
@@ -1,9 +1,19 @@
 import React, { ReactNode } from "react";
 
-function Chip({ children, accent }: { children: ReactNode; accent?: boolean }) {
+function Chip({
+  children,
+  accent,
+  offer,
+  className,
+}: {
+  children: ReactNode;
+  accent?: boolean;
+  offer?: boolean;
+  className?: string;
+}) {
   return (
     <span
-      className={`block w-fit rounded ${accent ? "mb-2 bg-main-yellow px-2 py-[6px] text-main-grey sm:mb-8" : "mb-4 bg-inherit text-main-grey-700"} text-sm font-bold uppercase tracking-[1.4px] text-main-grey`}
+      className={`block w-fit rounded ${className} ${accent ? "bg-main-yellow px-2 py-[6px] text-main-grey" : offer ? "bg-main-green-100 px-2 py-1 text-xs text-main-green" : "bg-inherit text-main-grey-700"} text-sm font-bold uppercase tracking-[1.4px]`}
     >
       {children}
     </span>
