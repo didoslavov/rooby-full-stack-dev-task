6394d03ba2ba64610cd0670b9e9b9ef882eb389b
didoslavov <didkoslawow@gmail.com>
2024-04-27 05:04:23 +0300

    fix styles

--- src/components/layout/Footer/Company.tsx
+++ src/components/layout/Footer/Company.tsx
@@ -5,7 +5,7 @@ function Company() {
   const data = getFooterLinks("company");
 
   return (
-    <article className="col-start-4 row-start-2 justify-self-end sm:col-start-6 lg:row-start-1 xl:col-start-5">
+    <article className="col-start-2 row-start-3 sm:col-start-4 md:row-start-2 md:justify-self-end lg:col-start-6 lg:row-start-1 xl:col-start-5">
       <NavContainer data={data} />
     </article>
   );
