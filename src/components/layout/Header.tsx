99b68c0f6bef4303846eb606a66912f130ec44f9
didoslavov <didkoslawow@gmail.com>
2024-04-27 05:05:12 +0300

    fix styles

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,6 +7,7 @@ import React, { useState } from "react";
 import logo from "/public/logo.svg";
 import NavLink from "../ui/NavLink";
 import Link from "next/link";
+import Button from "../ui/Button";
 
 const navLinks = ["Product", "Pricing", "Company", "Blog", "Contact"];
 
@@ -17,13 +18,15 @@ function Header() {
 
   return (
     <header className="relative col-start-1 col-end-13 grid grid-cols-12 items-center px-4 py-6 md:pl-4 2xl:col-start-2 2xl:col-end-12">
-      <Image
-        src={logo}
-        className={`${isOpen ? "left absolute top-6 z-50 m-4" : "col-span-2 col-start-1"}`}
-        alt="Company logo - Rooody"
-        width={95}
-        height={26}
-      />
+      <Link href="/">
+        <Image
+          src={logo}
+          className={`${isOpen ? "left absolute top-6 z-50 m-4" : "col-span-2 col-start-1"}`}
+          alt="Company logo - Rooody"
+          width={95}
+          height={26}
+        />
+      </Link>
       <ul
         className={`${isOpen ? "fixed right-0 top-0 flex h-screen w-screen flex-col items-center justify-center gap-10 bg-main-grey-100 text-2xl font-bold text-main-blue-900" : "hidden"} col-start-3 col-end-8 text-sm md:gap-4 lg:flex xl:gap-9`}
       >
@@ -38,20 +41,23 @@ function Header() {
           onClick={toggleMobileMenu}
         />
       </ul>
-      <Link
-        href="#"
-        className="col-span-3 col-start-4 text-nowrap rounded-[4px] bg-white px-4 py-2 text-center text-xs font-bold -tracking-[0.19px] transition-all duration-500 hover:bg-main-blue-900 hover:bg-opacity-20 sm:mr-4 md:col-span-2 md:col-start-8 lg:col-span-1 lg:col-start-11"
+      <Button
+        size="sm"
+        link="#"
+        type="outline"
+        className="col-span-3 col-start-4 text-nowrap px-5 text-center text-xs shadow-md md:col-span-2 md:col-start-8 lg:col-span-1 lg:col-start-10"
       >
         Log In
-      </Link>
-      <Link
-        href="#"
-        className="group col-span-3 col-start-8 rounded-[4px] bg-main-blue py-2 text-center text-xs font-bold -tracking-[0.19px] text-white transition-all duration-75 hover:skew-x-2 hover:skew-y-2 hover:scale-110 hover:bg-main-yellow hover:text-main-blue sm:col-span-2 sm:col-start-7 md:col-start-10 lg:col-start-12 xl:col-span-1"
+      </Button>
+
+      <Button
+        link="#"
+        size="sm"
+        className="col-span-3 col-start-7 row-span-1 text-nowrap px-6 text-center shadow-md md:col-start-10 lg:col-start-11"
+        type="full"
       >
-        <span className="group-hover:-skew-2 inline-block group-hover:-skew-y-2">
-          Try for Free
-        </span>
-      </Link>
+        Try for Free
+      </Button>
       <AiOutlineMenu
         onClick={toggleMobileMenu}
         size={30}
