c51278a174892ca5748ba09a4880d5c681fa5c9e
didoslavov <didkoslawow@gmail.com>
2024-04-28 00:46:14 +0300

    add main page in blog component

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,9 @@
+function page() {
+  return (
+    <section className="col-span-full grid grid-cols-subgrid">
+      <div className="col-start-2 col-end-12">ALL ARTICLES</div>
+    </section>
+  );
+}
+
+export default page;
