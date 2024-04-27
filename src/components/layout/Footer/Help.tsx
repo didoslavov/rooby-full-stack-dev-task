c5536b60ba48cf355428699d41037ede701cb733
didoslavov <didkoslawow@gmail.com>
2024-04-27 05:04:39 +0300

    fix styles

--- src/components/layout/Footer/Help.tsx
+++ src/components/layout/Footer/Help.tsx
@@ -6,7 +6,7 @@ function Help() {
   const data = getFooterLinks("help center");
 
   return (
-    <article className="col-start-8 row-start-2 text-nowrap sm:col-start-9 md:col-start-10 lg:col-start-11 lg:row-start-1 xl:col-start-10 xl:ml-10">
+    <article className="col-span-2 col-start-7 row-start-2 text-nowrap sm:col-start-9 sm:pl-2 md:col-start-10 md:pl-0 lg:col-start-11 lg:row-start-1 xl:col-start-10 xl:ml-10">
       <NavContainer data={data} />
     </article>
   );
