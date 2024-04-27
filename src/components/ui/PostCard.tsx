12eed79f7d07a08e36b21924d9379b9b2a7752a9
didoslavov <didkoslawow@gmail.com>
2024-04-28 02:09:44 +0300

    reuse post card in ui components

--- src/components/ui/PostCard.tsx
+++ src/components/ui/PostCard.tsx
@@ -1,42 +1,32 @@
 import Chip from "@/components/ui/Chip";
 import Heading from "@/components/ui/Heading";
-import Image, { StaticImageData } from "next/image";
+import { BlogPost } from "@/types/blog";
+import { formatDate } from "@/utils/formatDate";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-function Card({
-  className,
-  card,
-}: {
-  className?: string;
-  card: {
-    chip: string;
-    img: StaticImageData;
-    heading: string;
-    publishedAt: string;
-    author: string;
-  };
-}) {
+function PostCard({ className, card }: { className?: string; card: BlogPost }) {
   return (
     <article className={`${className}`}>
-      <Chip className="mb-4">{card.chip}</Chip>
+      <Chip className="mb-4">{card.tag}</Chip>
       <figure className="group border-t border-main-grey-700">
         <Image
-          src={card.img}
+          src={card.content.image}
           alt="Blog image"
           className="my-6 w-full group-hover:drop-shadow-xl"
         />
         <Heading className="">
           <h3 className="mb-6 text-lg md:leading-[42px] md:-tracking-[0.44px] lg:text-2xl xl:text-[32px]">
-            <Link href="">{card.heading}</Link>
+            <Link href="">{card.title}</Link>
           </h3>
         </Heading>
         <p>
           <span className="text-xs font-medium md:text-base">
-            {card.publishedAt}
+            {formatDate(card.date)}
           </span>
           <span className="text-xs font-medium text-main-grey-700 md:text-base">
-            ,by {card.author}
+            ,by {card.content.quote.author}
           </span>
         </p>
       </figure>
@@ -44,4 +34,4 @@ function Card({
   );
 }
 
-export default Card;
+export default PostCard;
