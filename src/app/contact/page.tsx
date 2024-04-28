fd53f9d0dfc0078f52528b11e2e75a94ad337f24
didoslavov <didkoslawow@gmail.com>
2024-04-28 19:14:50 +0300

    add embedded map api from google maps to show company location in contact form

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import Map from "@/components/layout/Contact/Map";
+import React from "react";
+
+function page() {
+  return (
+    <div className="col-start-2 col-end-5">
+      <Map />
+    </div>
+  );
+}
+
+export default page;
--- src/components/layout/Contact/Map.tsx
+++ src/components/layout/Contact/Map.tsx
@@ -1,15 +1,12 @@
-import React from "react";
+const API_KEY = process.env.EMBEDDED_MAPS_API_KEY;
 
 function Map() {
   return (
     <iframe
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2957.4245432804423!2d24.740869477276405!3d42.16259964755199!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14acd18de452a06b%3A0x838e93baa43989e5!2sSeveren%2C%20Vasil%20Levski%20St%2096%2C%204003%20Plovdiv!5e0!3m2!1sen!2sbg!4v1714209067958!5m2!1sen!2sbg"
-      width="100%"
-      height="288"
-      style={{ border: 0 }}
-      allowFullScreen={true}
+      width="600"
+      height="450"
       loading="lazy"
-      referrerPolicy="no-referrer-when-downgrade"
+      src={`https://www.google.com/maps/embed/v1/place?key=${API_KEY}&q=Vasil+Levski+96,Plovdiv+Bulgaria`}
     ></iframe>
   );
 }
