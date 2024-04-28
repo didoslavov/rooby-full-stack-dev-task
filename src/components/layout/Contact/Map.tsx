5cdd2e7e6a690ff1926d8ac3cd185e7cda7574f7
didoslavov <didkoslawow@gmail.com>
2024-04-29 01:28:50 +0300

    fix map size

--- src/components/layout/Contact/Map.tsx
+++ src/components/layout/Contact/Map.tsx
@@ -3,8 +3,9 @@ const API_KEY = process.env.EMBEDDED_MAPS_API_KEY;
 function Map() {
   return (
     <iframe
-      width="600"
-      height="450"
+      width="100%"
+      height="288"
+      className="outline-none"
       loading="lazy"
       src={`https://www.google.com/maps/embed/v1/place?key=${API_KEY}&q=Vasil+Levski+96,Plovdiv+Bulgaria`}
     ></iframe>
