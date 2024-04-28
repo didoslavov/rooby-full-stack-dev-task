f12e2396c5f6d524defad1a4fafa2d8fca2c2d74
didoslavov <didkoslawow@gmail.com>
2024-04-28 13:37:17 +0300

    add responsive styles

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,26 +5,44 @@ const posts = getAllPosts();
 
 function page() {
   return (
-    <section className="col-span-full grid grid-cols-subgrid bg-white py-[120px]">
-      <div className="col-start-2 col-end-12 mb-[120px] grid grid-cols-subgrid">
-        <PostCard className="col-start-1 col-end-6" post={posts[2]} big />
+    <section className="col-span-full grid grid-cols-subgrid bg-white py-12 lg:py-[120px]">
+      <div className="col-start-2 col-end-12 mb-12 grid grid-cols-subgrid lg:mb-[120px]">
+        <PostCard
+          className="col-start-1 col-end-12 md:col-end-6"
+          post={posts[2]}
+          big
+        />
       </div>
-      <div className="col-start-2 col-end-13 mb-[120px] grid grid-cols-10 gap-x-[30px] gap-y-[120px]">
+      <div className="col-start-2 col-end-13 mb-12 grid grid-cols-10 gap-x-[30px] gap-y-14 lg:mb-[120px] xl:gap-y-[120px]">
         {posts.slice(3, 9).map((p) => (
-          <PostCard small key={p.id} post={p} className="col-span-3" />
+          <PostCard
+            small
+            key={p.id}
+            post={p}
+            className="col-span-9 sm:col-span-3"
+          />
         ))}
       </div>
-      <div className="col-start-2 col-end-13 mb-[120px] grid grid-cols-subgrid">
-        <PostCard medium post={posts[9]} className="col-start-1 col-end-6" />
-        <div className="col-span-5 flex flex-col gap-7 self-center">
+      <div className="col-start-2 col-end-12 mb-12 grid grid-cols-subgrid lg:mb-[120px]">
+        <PostCard
+          medium
+          post={posts[9]}
+          className="col-start-1 col-end-12 mb-20 md:col-end-6 lg:mb-0"
+        />
+        <div className="col-span-full flex flex-col gap-7 md:col-span-5 lg:self-center">
           {posts.slice(10, 13).map((p) => (
             <PostCard key={p.id} post={p} />
           ))}
         </div>
       </div>
-      <div className="col-start-2 col-end-13 mb-8 grid grid-cols-10 gap-x-[30px] gap-y-[120px]">
+      <div className="col-span-full col-start-2 mb-8 grid grid-cols-10 gap-x-[30px] gap-y-7 md:col-end-13 md:gap-y-[120px]">
         {posts.slice(13, 16).map((p) => (
-          <PostCard small key={p.id} post={p} className="col-span-3" />
+          <PostCard
+            small
+            key={p.id}
+            post={p}
+            className="col-span-9 sm:col-span-3"
+          />
         ))}
       </div>
     </section>
