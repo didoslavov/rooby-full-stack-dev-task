defcb007a69207b45a7ee96ebe0c94cacfcb852c
didoslavov <didkoslawow@gmail.com>
2024-04-29 02:39:43 +0300

    add textarea input

---
+++ src/components/ui/TextArea.tsx
@@ -0,0 +1,28 @@
+import React, { ChangeEventHandler } from "react";
+
+function TextArea({
+  name,
+  placeholder,
+  className,
+  onChangeHandler,
+  value,
+}: {
+  name: string;
+  placeholder: string;
+  className?: string;
+  onChangeHandler?: ChangeEventHandler<HTMLTextAreaElement>;
+  value: string;
+}) {
+  return (
+    <textarea
+      name={name}
+      id={name}
+      placeholder={placeholder}
+      className={`resize-none rounded-[12px] p-4 pl-6 text-main-grey outline-none placeholder:text-[21px] placeholder:font-medium placeholder:-tracking-[0.29px] placeholder:text-main-grey-700 ${className}`}
+      onChange={onChangeHandler}
+      value={value}
+    />
+  );
+}
+
+export default TextArea;
