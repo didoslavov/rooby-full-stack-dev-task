cbe470b4cd5c8f1b11606d24eb8e55ce36bbc879
didoslavov <didkoslawow@gmail.com>
2024-04-28 04:02:19 +0300

    refactor post card component to render more abstract card based on a condition

--- src/components/ui/PostCard.tsx
+++ src/components/ui/PostCard.tsx
@@ -6,31 +6,65 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-function PostCard({ className, card }: { className?: string; card: BlogPost }) {
+/**
+ * @param {boolean} big changes layout of the card.
+ */
+
+function PostCard({
+  className,
+  post,
+  big,
+}: {
+  className?: string;
+  post: BlogPost;
+  big?: boolean;
+}) {
   return (
-    <article className={`${className}`}>
-      <Chip className="mb-4">{card.tag}</Chip>
-      <figure className="group border-t border-main-grey-700">
+    <>
+      <article
+        className={`${className} ${big && "max-h-[370px] self-center border-b border-t border-main-grey-700"}`}
+      >
+        {post.content.image && (
+          <Chip className={`mb-4 ${big && "mt-4"}`}>{post.tag}</Chip>
+        )}
+        <figure className={`group ${!big && "border-t border-main-grey-700"}`}>
+          {post.content.image && !big && (
+            <Image
+              src={post.content.image}
+              alt="Blog image"
+              className="my-6 w-full group-hover:drop-shadow-xl"
+            />
+          )}
+          <div className={`${big && "flex flex-col justify-between"}`}>
+            <Heading>
+              <h3
+                className={`${big ? "mb-[118px] text-[40px] leading-[50px] -tracking-[0.56px]" : "mb-6 text-lg md:leading-[42px] md:-tracking-[0.44px] lg:text-2xl xl:text-[32px]"}`}
+              >
+                <Link href="#">{post.title}</Link>
+              </h3>
+            </Heading>
+            <p className={`flex justify-between ${big && "mb-4"}`}>
+              <span>
+                <span className="text-xs font-medium md:text-base">
+                  {formatDate(post.date)}
+                </span>
+                <span className="text-xs font-medium text-main-grey-700 md:text-base">
+                  , by {post.content.quote.author}
+                </span>
+              </span>
+              {!post.content.image && <Chip className="mb-4">{post.tag}</Chip>}
+            </p>
+          </div>
+        </figure>
+      </article>
+      {big && (
         <Image
-          src={card.content.image}
+          src={post.content.image}
           alt="Blog image"
-          className="my-6 w-full group-hover:drop-shadow-xl"
+          className="col-start-6 col-end-12 my-6 group-hover:drop-shadow-xl"
         />
-        <Heading className="">
-          <h3 className="mb-6 text-lg md:leading-[42px] md:-tracking-[0.44px] lg:text-2xl xl:text-[32px]">
-            <Link href="">{card.title}</Link>
-          </h3>
-        </Heading>
-        <p>
-          <span className="text-xs font-medium md:text-base">
-            {formatDate(card.date)}
-          </span>
-          <span className="text-xs font-medium text-main-grey-700 md:text-base">
-            ,by {card.content.quote.author}
-          </span>
-        </p>
-      </figure>
-    </article>
+      )}
+    </>
   );
 }
 
