bb42d5e4bf573b003b6ec5a43cf70b9bbd27ff0d
didoslavov <didkoslawow@gmail.com>
2024-04-28 04:02:48 +0300

    render big post card in blog route

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,14 @@
+import PostCard from "@/components/ui/PostCard";
+import { getAllPosts } from "@/data/blogPosts";
+
+const posts = getAllPosts();
+
 function page() {
   return (
-    <section className="col-span-full grid grid-cols-subgrid">
-      <div className="col-start-2 col-end-12">ALL ARTICLES</div>
+    <section className="col-span-full grid grid-cols-subgrid bg-white py-[120px]">
+      <div className="col-start-2 col-end-12 grid grid-cols-subgrid">
+        <PostCard className="col-start-1 col-end-6" post={posts[2]} big />
+      </div>
     </section>
   );
 }
