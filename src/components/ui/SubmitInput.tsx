4c046ea3d396675e768c42b2a9c55b61ce7a20ea
didoslavov <didkoslawow@gmail.com>
2024-04-29 02:40:34 +0300

    fix styles

--- src/components/ui/SubmitInput.tsx
+++ src/components/ui/SubmitInput.tsx
@@ -12,7 +12,7 @@ function SubmitInput({
 }) {
   return (
     <div
-      className={`${className} ${green ? "flex items-center bg-main-green px-2 transition-all duration-500 hover:bg-main-green-100 hover:text-black hover:shadow-xl" : "bg-main-blue hover:bg-main-yellow hover:text-main-blue"} overflow-hidden rounded-[8px] text-white transition-all duration-500 hover:cursor-pointer`}
+      className={`${green ? "flex items-center bg-main-green px-2 transition-all duration-500 hover:bg-main-green-100 hover:text-black hover:shadow-xl" : "bg-main-blue hover:bg-main-yellow hover:text-main-blue"} overflow-hidden rounded-[8px] text-white transition-all duration-500 hover:cursor-pointer ${className}`}
     >
       <input
         type="submit"
