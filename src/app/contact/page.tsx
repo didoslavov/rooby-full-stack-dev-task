2338abe1f433228601a4633121ee8811da4d94cb
didoslavov <didkoslawow@gmail.com>
2024-04-29 02:39:00 +0300

    fix padding

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,12 +1,8 @@
 import Contact from "@/components/layout/Contact/Contact";
-import Map from "@/components/layout/Contact/Map";
-import ContactInfo from "@/components/ui/ContactInfo";
-import Heading from "@/components/ui/Heading";
-import React from "react";
 
 function page() {
   return (
-    <section className="col-span-full grid grid-cols-subgrid py-[72px]">
+    <section className="col-span-full grid grid-cols-subgrid pt-[72px]">
       <Contact />
     </section>
   );
