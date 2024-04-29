4f36fb780834e8b81fc0552d292038890e810ae3
didoslavov <didkoslawow@gmail.com>
2024-04-29 15:03:24 +0300

    fix styles

--- src/components/ui/Notification.tsx
+++ src/components/ui/Notification.tsx
@@ -12,7 +12,7 @@ function Notification({
 }) {
   return (
     <div
-      className={`${type === "error" ? "text-main-red-900 bg-main-grey-200" : "bg-main-blue-900 text-white"} border-dark-blue fixed right-8 top-10 z-50 rounded-lg border px-6 py-4 text-xs md:text-base xl:px-10 xl:py-8 xl:text-xl`}
+      className={`${type === "error" ? "text-main-red-900 bg-main-grey-200" : "text-main-green-900 bg-main-green-100"} border-dark-blue fixed right-8 top-10 z-50 rounded-lg border px-6 py-4 text-xs md:text-base xl:px-10 xl:py-8 xl:text-xl`}
     >
       <IoMdClose
         className="text-dark-blue absolute right-1 top-1 hover:cursor-pointer xl:right-2 xl:top-2 xl:text-2xl"
